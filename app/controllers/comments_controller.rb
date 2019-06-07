acf8a06d32a17469fcac8c8c13bfc749beaafdd2
Nicolás Nisoria <nicolas.nisoria@gmail.com>
2019-06-07 03:33:58 -0300

    Finish comments feature

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,16 @@
+class CommentsController < ApplicationController
+    include CommentsHelper
+
+    def create
+        @comment = Comment.new(comment_params)
+        @comment.article_id = params[:article_id]
+        if @comment.save
+          flash[:success] = "Comment successfully created"
+          redirect_to article_path(@comment.article)
+        else
+          flash[:error] = "Something went wrong"
+          render 'new'
+        end
+    end
+
+end
