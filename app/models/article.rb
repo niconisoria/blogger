d27cee545bdac5d35d7bd0c31e1560d4530c98db
Nicolás Nisoria <nicolas.nisoria@gmail.com>
2019-06-08 11:34:20 -0300

    Fix dependant destroy

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
-    has_many :comments
+    has_many :comments, dependent: :destroy
     has_many :taggings, dependent: :destroy
     has_many :tags, through: :taggings
     has_one_attached :image
